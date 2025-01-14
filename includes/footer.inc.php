a303a903c45430c52c0abff1511893a7e0ac467d
GFerros <gaferris1@gmail.com>
2025-01-13 20:22:09 -0500

    updated footer copyright for current year

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -7,7 +7,7 @@ Holds the footer used for each page
             <div class="container">
                 <div class="row">
                     <div class="col-12 pt-3">
-                        <p>Grant Ferris &copy; 2024</p>
+                        <p>Grant Ferris &copy; 2025</p>
                         <p>Made using Bootstrap | JQuery | PHP </p>
                     </div>
                 </div>
